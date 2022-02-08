e311a2457c45b3c87abf70c0748d26b59b100e67
sindrelothe <sindrl@online.no>
2022-02-08 11:16:16 +0100

    initial commit

---
+++ Assignment 2/code.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+
+def f1(x: int):
+    return x * 2
+
+
+print(f1(2))
---
+++ TDT4171/tests/test_tdt4171.py
@@ -0,0 +1,5 @@
+from tdt4171 import __version__
+
+
+def test_version():
+    assert __version__ == '0.1.0'
