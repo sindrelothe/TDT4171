ad4f0cc6154e1c538879f256b7e24490d95d0d3b
sindrelothe <sindrl@online.no>
2022-02-28 17:14:49 +0100

    did alot of stuff

--- Assignment 4/DecisionTree.py
+++ Assignment 4/DecisionTree.py
@@ -1,19 +1,39 @@
 import numpy as np
 from typing import List
-import os
+
+
+class Node:
+    def __init__(self, definition: np.ndarray):
+        self.type: int = definition[-1]
+        self.attributes: np.ndarray = definition[0:-1]
+        self.parent: Node = None
+        self.children: List[Node] = []
+
+    def __repr__(self):
+        return f"type={self.type}, attributes={self.attributes}"
 
 
 class DecisionTree:
     def __init__(self, example_filename: str, test_filename: str, path: str):
         self.examples: np.ndarray = np.genfromtxt(
-            path + example_filename, delimiter=","
+            path + example_filename, delimiter=",", dtype="int64"
         )
         self.test_values: np.ndarray = np.genfromtxt(
-            path + test_filename, delimiter=","
+            path + test_filename, delimiter=",", dtype="int64"
         )
+
+        self.node_examples_list: List[Node] = [Node(ex) for ex in self.examples]
+        self.node_test_list: List[Node] = [Node(test) for test in self.test_values]
+
+        attributes: List[int] = [
+            n for n in range(len(self.node_examples_list[0].attributes))
+        ]
 
     def randomImportance(self):
         pass
+
+    def BFunction(self, q: float) -> float:
+        return -(q * np.log2(q) + (1 - q) * np.log2(1 - q))
 
     def learn(
         self, examples: np.ndarray, attributes: np.ndarray, parent_examples: np.ndarray
@@ -27,8 +47,9 @@ def main():
     test = "test.csv"
     train = "train.csv"
     tree: DecisionTree = DecisionTree(train, test, path)
-
-    print(tree.examples.var())
+    n: Node = Node(tree.examples[0])
+    for n in tree.node_examples_list:
+        print(n.attributes)
 
 
 if __name__ == "__main__":
